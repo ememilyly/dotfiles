3ddc55541a455dc3ee7da9c67d578c80772378c5
emily <git@emily.ly>
2023-03-11 15:16:23 +0000

    another zsh highlighting location

--- .config/zsh/zsh-syntax-highlighting.zsh
+++ .config/zsh/zsh-syntax-highlighting.zsh
@@ -1,9 +1,11 @@
 # Enable Zsh command highlighting
-source_file="share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-if [[ -f "$HOME/.local/$source_file" ]]; then
-    source "$HOME/.local/$source_file"
-elif [[ -f "/usr/local/$source_file" ]]; then
-    source "/usr/local/$source_file"
+source_file="zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+if [[ -f "$HOME/.local/share/$source_file" ]]; then
+    source "$HOME/.local/share/$source_file"
+elif [[ -f "/usr/local/share/$source_file" ]]; then
+    source "/usr/local/share/$source_file"
+elif [[ -f "/usr/share/zsh/plugins/$source_file" ]]; then
+    source "/usr/share/zsh/plugins/$source_file"
 else
     echo "Can't find zsh-syntax-highlighting.zsh"
  fi
